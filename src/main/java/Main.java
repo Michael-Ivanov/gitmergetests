4f3a951cfaaa6dea42539fa9ae22664a645586c1
Michael Ivanov <rattys@yandex.ru>
2022-05-09 11:29:36 +0300

    dev: initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
+        System.out.println("running...");
+    }
+}
